9e74024297370b892fd871f79ad54acf700865c7
aalaaEbraheem <aalaa.ibraheem.private@gmail.com>
2023-04-29 23:43:36 +0300

    Expert System

---
+++ ExpertSystem/externalsymp.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ExpertSystem
+{
+    public partial class externalsymp : Form
+    {
+        public externalsymp()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgnButton_Click(object sender, EventArgs e)
+        {
+            if (swelleyeschecheckBox.Checked)
+            {
+                resultpanel.Controls.Clear();
+               externalresult a = new externalresult();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (swelleyeschecheckBox.Checked || s.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresult a = new externalresult();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (swelleyeschecheckBox.Checked || s.Checked || burningsensationcheckBox.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresult a = new externalresult();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+
+            if (burningsensationcheckBox.Checked || CheckBoxbump.Checked || blurryvisioncheckBox.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresult a = new externalresult();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (swelleyeschecheckBox.Checked || s.Checked || burningsensationcheckBox.Checked || CheckBoxbump.Checked || blurryvisioncheckBox.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresult a = new externalresult();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (CheckBoxbump.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresultb a = new externalresultb();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (CheckBoxbump.Checked || checkBoxitching.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresultb a = new externalresultb();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (CheckBoxbump.Checked || checkBoxitching.Checked || checkBoxrashes.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresultb a = new externalresultb();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+
+            if (CheckBoxbump.Checked && checkBoxitching.Checked && checkBoxrashes.Checked && checkBoxblister.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresultb a = new externalresultb();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+            if (checkBoxblister.Checked)
+            {
+                resultpanel.Controls.Clear();
+                externalresultb a = new externalresultb();
+                a.TopLevel = false;
+                resultpanel.Controls.Add(a);
+                a.Show();
+
+            }
+
+        }
+    }
+}
